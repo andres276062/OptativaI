69cd2c12e1a922f5c1b560cc8581831e23e0d7bb
Andres Buitrago <andresbuitrago274062@correo.itm.edu.co>
2023-10-23 13:04:50 -0500

    Ejercicio añadido

---
+++ StringLength/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("String Length");
+
+        Console.Write("Ingresa una palabra: ");
+        string palabra = Console.ReadLine();
+
+        int longitud = palabra.Length;
+
+        Console.WriteLine($"Resultado: {longitud}");
+    }
+}
