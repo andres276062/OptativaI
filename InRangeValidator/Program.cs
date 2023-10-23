5c8a0657e15c581836d4c8b0dcddb202f170d5be
Andres Buitrago <andresbuitrago274062@correo.itm.edu.co>
2023-10-23 13:20:02 -0500

    Ejercicio añadido

---
+++ InRangeValidator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("InRange Validator");
+
+        Console.Write("Ingresa un número: ");
+        if (int.TryParse(Console.ReadLine(), out int numero))
+        {
+            if (numero >= 10 && numero <= 20)
+            {
+                Console.WriteLine("Resultado: Está en el rango.");
+            }
+            else
+            {
+                Console.WriteLine("Resultado: Fuera del rango.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("El valor ingresado no es válido. Debes ingresar un número entero.");
+        }
+    }
+}
