ba329a90839dd2ac329bb92cc06286588811bc69
Andres Buitrago <andresbuitrago274062@correo.itm.edu.co>
2023-10-23 12:57:21 -0500

    Ejercicio añadido

---
+++ RemainderFinder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Remainder Finder");
+        
+        // Solicita al usuario dos números
+        Console.Write("Ingresa el primer número: ");
+        if (int.TryParse(Console.ReadLine(), out int numero1))
+        {
+            Console.Write("Ingresa el segundo número: ");
+            if (int.TryParse(Console.ReadLine(), out int numero2))
+            {
+                // Calcula el residuo de la división
+                int residuo = numero1 % numero2;
+                
+                Console.WriteLine($"Resultado: {residuo}");
+            }
+            else
+            {
+                Console.WriteLine("El segundo número ingresado no es válido. Debe ser un número entero.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("El primer número ingresado no es válido. Debe ser un número entero.");
+        }
+    }
+}
+
