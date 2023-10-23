570b3ab9207d732a8b9c1ec7e6681ab21737b8f3
Andres Buitrago <andresbuitrago274062@correo.itm.edu.co>
2023-10-23 13:16:00 -0500

    Ejercicio añadido

---
+++ VowelCounter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Vowel Counter");
+
+        Console.Write("Ingresa una palabra: ");
+        string palabra = Console.ReadLine().ToLower(); // Convertir la palabra a minúsculas para facilitar la comparación.
+
+        int contadorVocales = 0;
+
+        foreach (char letra in palabra)
+        {
+            if ("aeiou".Contains(letra))
+            {
+                contadorVocales++;
+            }
+        }
+
+        Console.WriteLine($"Resultado: {contadorVocales}");
+    }
+}
