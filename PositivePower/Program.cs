c6b47a2f721e582ea4e4de40f837f3019c3113b9
Andres Buitrago <andresbuitrago274062@correo.itm.edu.co>
2023-10-23 09:26:13 -0500

    Ejercicio añadido

--- PositivePower/Program.cs
+++ PositivePower/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Positive Power");
+        Console.Write("Ingresa un número: ");
+        string userInput = Console.ReadLine();
+
+        if (double.TryParse(userInput, out double number))
+        {
+            if (number >= 0)
+            {
+                double result = Math.Pow(number, 2);
+                Console.WriteLine($"Resultado: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Resultado: Número negativo.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Entrada no válida. Debes ingresar un número válido.");
+        }
+    }
+}
