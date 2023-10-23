0c0dce4098884ba05e67fda0a0d6e91cc92ffb59
Andres Buitrago <andresbuitrago274062@correo.itm.edu.co>
2023-10-23 13:07:09 -0500

    Ejercicio añadido

---
+++ AverageOfFour/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Average of Four");
+
+        // Solicita al usuario cuatro números
+        Console.Write("Ingresa el primer número: ");
+        if (double.TryParse(Console.ReadLine(), out double numero1))
+        {
+            Console.Write("Ingresa el segundo número: ");
+            if (double.TryParse(Console.ReadLine(), out double numero2))
+            {
+                Console.Write("Ingresa el tercer número: ");
+                if (double.TryParse(Console.ReadLine(), out double numero3))
+                {
+                    Console.Write("Ingresa el cuarto número: ");
+                    if (double.TryParse(Console.ReadLine(), out double numero4))
+                    {
+                        // Calcula el promedio de los cuatro números
+                        double promedio = (numero1 + numero2 + numero3 + numero4) / 4;
+
+                        Console.WriteLine($"Resultado: {promedio:F2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El cuarto número ingresado no es válido. Debe ser un número.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("El tercer número ingresado no es válido. Debe ser un número.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("El segundo número ingresado no es válido. Debe ser un número.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("El primer número ingresado no es válido. Debe ser un número.");
+        }
+    }
+}
+
