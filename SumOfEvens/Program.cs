1fd78ec7fb20f11e50bab3a432ff641b53fcc7c4
Andres Buitrago <andresbuitrago274062@correo.itm.edu.co>
2023-10-23 12:59:43 -0500

    Ejercicio añadido

---
+++ SumOfEvens/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Sum of Evens");
+
+        int sumaPares = 0;
+
+        for (int i = 2; i <= 50; i += 2)
+        {
+            sumaPares += i;
+        }
+
+        Console.WriteLine($"Resultado: {sumaPares}");
+    }
+}
+
